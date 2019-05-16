a529bd365d9c36fd79716d54cf8c5a9cfd513787
Robert Jackson <rjackson@linkedin.com>
2019-05-16 19:59:40 -0400

    Add "accept a nooping function" style API.

--- tests/fakeLifeLineTest.js
+++ tests/fakeLifeLineTest.js
@@ -1,11 +1,5 @@
 // @ts-nocheck
-const {
-  cancellableRace,
-  withRaceSettled,
-  throwIfCancelled,
-  withRaceCancellableTask,
-  disposiblePromise,
-} = require("race-cancellation");
+const { cancellableRace, throwIfCancelled } = require("race-cancellation");
 
 // BEGIN simulated ember-lifeline APIs
 let CLEANUP = new WeakMap();
@@ -26,13 +20,6 @@ function runDestroyables(obj) {
   }
 }
 
-async function entangle(obj, promise) {
-  const [raceCancellation, cancel] = cancellableRace();
-
-  addDestroyable(obj, cancel);
-
-  return throwIfCancelled(await raceCancellation(() => promise));
-}
 // END simulated ember-lifeline APIS
 
 // fake object model with a builtin destroy method
@@ -46,69 +33,146 @@ class CoolBaseClass {
   }
 }
 
-QUnit.module("Promises entangled with object lifetime", (hooks) => {
+QUnit.module("Promises entangled with object lifetime", () => {
   function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-  QUnit.test("returns a promise that functions properly when not destroyed", async function(assert) {
-    class Child extends CoolBaseClass {
-      doStuff() {
-        return entangle(this, sleep(50)).then(() => assert.step('promise resolved'));
-      }
+  QUnit.module("entangling function callback lazily", function() {
+    async function entangle(obj, promise, executor) {
+      const [raceCancellation, cancel] = cancellableRace();
+
+      addDestroyable(obj, cancel);
+
+      return promise.then(
+        async result => await raceCancellation(() => executor(result))
+      );
     }
 
-    let obj = new Child();
-    
-    await obj.doStuff();
+    QUnit.test(
+      "returns a promise that functions properly when not destroyed",
+      async function(assert) {
+        class Child extends CoolBaseClass {
+          doStuff() {
+            return entangle(this, sleep(50), () =>
+              assert.step("promise resolved")
+            );
+          }
+        }
 
-    await sleep(100);
-    
-    assert.verifySteps(['promise resolved']);
+        let obj = new Child();
+
+        await obj.doStuff();
+
+        await sleep(100);
+
+        assert.verifySteps(["promise resolved"]);
+      }
+    );
+
+    QUnit.test("it can avoid work pending when destroyed", async function(
+      assert
+    ) {
+      class Child extends CoolBaseClass {
+        doStuff() {
+          return entangle(this, sleep(50), () =>
+            assert.step("promise resolved")
+          );
+        }
+      }
+
+      let obj = new Child();
+
+      setTimeout(() => obj.destroy(), 5);
+
+      await obj.doStuff();
+
+      await sleep(100);
+
+      assert.verifySteps([]);
+    });
   });
 
-  QUnit.test("it can avoid work pending when destroyed", async function(assert) {
-    class Child extends CoolBaseClass {
-      doStuff() {
-        return entangle(this, sleep(50)).then(() => assert.step('promise resolved'));
+  QUnit.module("entangling promise directly", function() {
+    async function entangle(obj, promise) {
+      const [raceCancellation, cancel] = cancellableRace();
+
+      addDestroyable(obj, cancel);
+
+      return throwIfCancelled(await raceCancellation(() => promise));
+    }
+
+    QUnit.test(
+      "returns a promise that functions properly when not destroyed",
+      async function(assert) {
+        class Child extends CoolBaseClass {
+          doStuff() {
+            return entangle(this, sleep(50)).then(() =>
+              assert.step("promise resolved")
+            );
+          }
+        }
+
+        let obj = new Child();
+
+        await obj.doStuff();
+
+        await sleep(100);
+
+        assert.verifySteps(["promise resolved"]);
       }
-    }
+    );
 
-    let obj = new Child();
-    
-    setTimeout(() => obj.destroy(), 5);
-
-    try {
-      await obj.doStuff();
-    } catch(e) {
-      // 🤔
-    }
-
-    await sleep(100);
-
-    assert.verifySteps([]);
-  });
-
-  QUnit.test("any finalization work can still be done when destroyed", async function(assert) {
-    class Child extends CoolBaseClass {
-      doStuff() {
-        return entangle(this, sleep(15))
-          .then(() => assert.step('promise resolved'))
-          .finally(() => assert.step('finally called'));
+    QUnit.test("it can avoid work pending when destroyed", async function(
+      assert
+    ) {
+      class Child extends CoolBaseClass {
+        doStuff() {
+          return entangle(this, sleep(50)).then(() =>
+            assert.step("promise resolved")
+          );
+        }
       }
-    }
 
-    let obj = new Child();
-    
-    setTimeout(() => obj.destroy(), 5);
+      let obj = new Child();
 
-    try {
-      await obj.doStuff();
-    } catch(e) {
-      // 🤔
-    }
-    await sleep(100);
-    
-    assert.verifySteps(['finally called']);
+      setTimeout(() => obj.destroy(), 5);
+
+      try {
+        await obj.doStuff();
+      } catch (e) {
+        // 🤔
+      }
+
+      await sleep(100);
+
+      assert.verifySteps([]);
+    });
+
+    QUnit.test(
+      "any finalization work can still be done when destroyed",
+      async function(assert) {
+        class Child extends CoolBaseClass {
+          doStuff() {
+            return entangle(this, sleep(15))
+              .then(() => assert.step("promise resolved"))
+              .finally(() => assert.step("finally called"));
+          }
+        }
+
+        let obj = new Child();
+
+        setTimeout(() => obj.destroy(), 5);
+
+        try {
+          await obj.doStuff();
+        } catch (e) {
+          // 🤔
+        }
+        await sleep(100);
+
+        assert.verifySteps(["finally called"]);
+      }
+    );
   });
 });
